bc5d6615b6cec4a543dc119ba0a2820fa7737ac7
문예성 <moon_ye_seong@mun-yeseong-ui-MacBook-Pro.local>
2019-01-24 14:04:31 +0900

    hello world!

--- app/routes/HomePage.js
+++ app/routes/HomePage.js
@@ -13,26 +13,90 @@ class HomePage extends Component {
     return (
       <div>
 
-        <div style={{ color: "#6b6161" }}>
+        <div style={{ color: "#fa3b3b" }}>
+          <div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                height: "100px",
+                alignItems: "center",
+                backgroundColor: "#fcecd7",
+                borderRadius: "4px"
+              }}
+            >
+              <span>@.@</span>
+            </div>
+          </div>
           <div
             style={{
               height: "100px",
-              backgroundColor: "#fcecd7",
-              borderRadius: "4px",
+              backgroundColor: "#fdcdce",
               textAlign: "center",
               paddingTop: "40px",
-              paddingBottom: "40px"
+              paddingBottom: "40px",
+              borderRadius: "3px",
+              display: "block"
             }}
           >
             <span
               style={{
                 lineHeight: "20px",
-                backgroundColor: "#b8e986",
-                color: "#4a90e2"
+                backgroundColor: "rgba(255, 243, 241, 0.84)",
+                color: "#fc4f4f",
+                fontSize: "56px",
+                fontWeight: 306,
+                fontStyle: "oblique",
+                padding: "0px",
+                paddingLeft: "2px",
+                paddingRight: "11px"
               }}
             >
-              Hello Colorful World 🖍🖍
+              Hello world!
             </span>
+            <h2 style={{ padding: "21px" }}>
+              <span
+                style={{
+                  color: "rgba(240, 231, 174, 0.84)",
+                  backgroundColor: "rgba(247, 37, 37, 0.84)",
+                  fontSize: "28px",
+                  fontStyle: "oblique",
+                  paddingLeft: "5px",
+                  paddingRight: "5px",
+                  paddingBottom: "5px",
+                  padding: "7px",
+                  fontWeight: 200
+                }}
+              >
+                I'm yeseong
+              </span>
+              <h3 />
+              <div />
+              <article />
+            </h2>
+            <main />
+          </div>
+          <div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                height: "100px",
+                alignItems: "center",
+                backgroundColor: "#fcecd7",
+                borderRadius: "4px"
+              }}
+            >
+              <span
+                style={{
+                  fontSize: "17px",
+                  paddingBottom: "-3px",
+                  marginBottom: "-10px"
+                }}
+              >
+                2019.01
+              </span>
+            </div>
           </div>
         </div>
 
