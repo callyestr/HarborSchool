58ee9b0cd8e8551c84cdba99a9232c69f516fc2c
문예성 <moon_ye_seong@mun-yeseong-ui-MacBook-Pro.local>
2019-02-12 21:25:02 +0900

    05Day_1

--- app/routes/Page6.js
+++ app/routes/Page6.js
@@ -6,6 +6,8 @@
 
 import React, { Component } from "react";
 
+import { Row, Column } from "modules/Layout";
+
 class Page6 extends Component {
   // eslint-disable-line react/prefer-stateless-function
 
@@ -15,103 +17,233 @@ class Page6 extends Component {
 
         <div
           style={{
-            backgroundColor: "#ffffff",
-            height: "30px",
-            display: "block",
-            position: "relative"
+            paddingTop: "50px",
+            paddingRight: "50px",
+            paddingBottom: "50px",
+            paddingLeft: "50px",
+            backgroundColor: "#ffffff"
           }}
         >
-          <span />
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "5%",
-              left: "0px"
-            }}
-          >
-            <span />
-          </div>
-          <div
-            style={{
-              height: "30px",
-              width: "20%",
-              position: "absolute",
-              left: "55px",
-              marginLeft: "1%"
-            }}
-          >
-            <img
-              src="https://amp.businessinsider.com/images/59a59a8d79bbfd1d008b601a-960-480.png"
-              style={{ width: "68px", height: "100%", marginLeft: "15px" }}
-            />
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "45%",
-              left: "25%"
-            }}
-          >
-            <span />
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "3%",
-              right: "25%"
-            }}
-          >
-            <span />
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "3%",
-              right: "20%"
-            }}
-          >
-            <span />
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "3%",
-              right: "15%"
-            }}
-          >
-            <span />
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "3%",
-              right: "10%"
-            }}
-          >
-            <span />
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "#e0cece",
-              height: "30px",
-              width: "7%",
-              right: "0%"
-            }}
-          >
-            <span />
+          <div style={{ position: "relative" }}>
+            <Row style={{ backgroundColor: "#ffffff", height: "40px" }}>
+              <Column col={2} style={{ backgroundColor: "#918080" }} />
+              <Column col={2} style={{ backgroundColor: "#ffffff" }} />
+              <Column col={2} style={{ backgroundColor: "#918080" }} />
+              <Column col={1} style={{ backgroundColor: "#ffffff" }} />
+              <Column col={2} style={{ backgroundColor: "#918080" }} />
+            </Row>
+            <Column style={{ height: "80px", backgroundColor: "#ffffff" }} />
+            <div />
+            <div>
+              <Row style={{ height: "100%" }}>
+                <Column col={6}>
+                  <Row>
+                    <Column
+                      col={8}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        height: "150px",
+                        position: "relative",
+                        paddingRight: "20px",
+                        paddingBottom: "10px"
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          backgroundColor: "#555555",
+                          position: "relative"
+                        }}
+                      >
+                        <span
+                          style={{
+                            position: "absolute",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            top: "50%",
+                            backgroundColor: "#c76363",
+                            fontSize: "97px"
+                          }}
+                        >
+                          SURF
+                        </span>
+                      </div>
+                      <div
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          backgroundColor: "#ffffff"
+                        }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        paddingBottom: "10px",
+                        paddingRight: "15px"
+                      }}
+                    >
+                      <div
+                        style={{ height: "100%", backgroundColor: "#d0021b" }}
+                      />
+                    </Column>
+                    <div />
+                    <Column
+                      col={8}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        height: "100px",
+                        paddingRight: "20px",
+                        paddingTop: "10px"
+                      }}
+                    >
+                      <div
+                        style={{
+                          height: "100px",
+                          backgroundColor: "#131313",
+                          paddingTop: "5px"
+                        }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        height: "100px",
+                        paddingTop: "10px",
+                        paddingRight: "15px"
+                      }}
+                    >
+                      <div
+                        style={{ backgroundColor: "#7e7e7e", height: "100px" }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        height: "200px",
+                        paddingTop: "20px",
+                        backgroundColor: "#ffffff",
+                        paddingRight: "15px"
+                      }}
+                    >
+                      <div
+                        style={{ height: "200px", backgroundColor: "#b82e2e" }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        height: "200px",
+                        paddingLeft: "5px",
+                        paddingTop: "20px",
+                        paddingRight: "20px"
+                      }}
+                    >
+                      <div
+                        style={{ height: "200px", backgroundColor: "#b92626" }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        height: "200px",
+                        paddingTop: "20px",
+                        paddingRight: "15px"
+                      }}
+                    >
+                      <div
+                        style={{ height: "200px", backgroundColor: "#be3030" }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{ height: "80px", backgroundColor: "#ffffff" }}
+                    >
+                      <div
+                        style={{
+                          backgroundColor: "#c5b5b5",
+                          height: "80px",
+                          marginTop: "20px",
+                          marginRight: "15px"
+                        }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        height: "80px",
+                        paddingLeft: "5px",
+                        paddingTop: "20px",
+                        paddingRight: "20px",
+                        backgroundColor: "#ffffff"
+                      }}
+                    >
+                      <div
+                        style={{ height: "80px", backgroundColor: "#b1a3a3" }}
+                      />
+                    </Column>
+                    <Column
+                      col={4}
+                      style={{
+                        height: "80px",
+                        paddingTop: "20px",
+                        paddingRight: "15px",
+                        backgroundColor: "#ffffff"
+                      }}
+                    >
+                      <div
+                        style={{ height: "70px", backgroundColor: "#bbacac" }}
+                      />
+                    </Column>
+                  </Row>
+                </Column>
+                <Column col={1} style={{ backgroundColor: "#ffffff" }}>
+                  <Row />
+                </Column>
+                <Column
+                  col={5}
+                  style={{ backgroundColor: "rgba(155, 155, 155, 0.25)" }}
+                >
+                  <Row />
+                </Column>
+                <div />
+                <Column
+                  col={6}
+                  style={{
+                    height: "300px",
+                    backgroundColor: "#ffffff",
+                    position: "relative"
+                  }}
+                >
+                  <Row />
+                  <div
+                    style={{
+                      height: "60px",
+                      width: "60px",
+                      backgroundColor: "#bbb2b2",
+                      position: "absolute",
+                      right: "0px",
+                      bottom: "0px"
+                    }}
+                  />
+                </Column>
+                <Column col={1} style={{ backgroundColor: "#ffffff" }} />
+                <Column
+                  col={5}
+                  style={{
+                    backgroundColor: "rgba(155, 155, 155, 0.25)",
+                    height: "300px"
+                  }}
+                >
+                  <Row />
+                </Column>
+              </Row>
+            </div>
           </div>
         </div>
 
